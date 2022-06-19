9917508a3162ee128e0cc5a9e9ee0983329f93f2
levataties <43945494+levataties@users.noreply.github.com>
2022-06-19 00:58:09 -0500

    daba muchos pedos tu arrow carrot en lo responsive se veia mal en mobile fuera de margen y demas. Ya llevo casi 2 horas tratando de arreglar con estilos pero ya lo quite mejor

--- src/components/carrousel/carrousel.jsx
+++ src/components/carrousel/carrousel.jsx
@@ -3,11 +3,6 @@ import React, { Fragment } from "react";
 import "./carrousel.styles.css"
 import {Link} from "react-router-dom";
 
-    const objetoIcons = {
-    nextIcon: <span className="arrow_carrot-next"></span>,
-    prevIcon: <span className="arrow_carrot-prev"></span>,
-    };
-
     const Carrousel = ({totalPosts}) => {
 
         const postsToDisplay = totalPosts.slice(0,4);
@@ -16,8 +11,7 @@ import {Link} from "react-router-dom";
                 <Carousel
                     fade
                     interval={3000}
-                    nextIcon={objetoIcons.nextIcon}
-                prevIcon={objetoIcons.prevIcon}>
+                    >
                 {postsToDisplay.map(({ title, desc, image, _id }) => (
                     <Carousel.Item key={_id}>
                         <img className="image-resize" src={image} alt="slide show" />
