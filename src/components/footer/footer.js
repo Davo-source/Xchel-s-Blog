8afd550b0cd35cc8971e91ed32e10cacb1f334d6
levataties <43945494+levataties@users.noreply.github.com>
2022-10-17 14:59:27 -0500

    se agrego un footer sencillo de xchel dev y un pequeno ajuste al tamano de la imagen colocada

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,18 @@
 import React from "react"
 import "./footer.css"
+import {Typography} from "@mui/material";
 
     const Footer = () => {
 
     return (
-        <footer>
-            Footer
-        </footer>
+        <section className= "footer-section">
+            <hr/>
+            <section className= "d-flex justify-content-center">
+                <Typography variant="subtitle2">
+                    Xchel developer &copy; 2022
+                </Typography>
+            </section>
+        </section>
     )
     }
 
